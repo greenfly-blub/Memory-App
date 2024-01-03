f8e6acd1fa41db8accf87227cc14dba1f8b2285a
greenfly-blub <63907390+greenfly-blub@users.noreply.github.com>
2024-01-03 14:15:00 +0100

    End Activity mit Berechnung (derzeit wird Pos 3 abgezogen) und Anzeige im End Screen

--- app/src/main/java/com/example/alzheimer_project/End_Activity.java
+++ app/src/main/java/com/example/alzheimer_project/End_Activity.java
@@ -1,6 +1,48 @@
 package com.example.alzheimer_project;
 
-public class End_Activity
-{
+import android.os.Bundle;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class End_Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end);
+
+        // Get the List with the numbers from the activity before
+        ArrayList<Integer> numbers = getIntent().getIntegerArrayListExtra("randomNumbers");
+
+
+        // Call Add and Substract mehtod
+        int endResult = addAndSubstract(numbers);
+
+        // Show End Result
+        TextView specialSumTextView = findViewById(R.id.tv_math);
+        specialSumTextView.setText("Ergebnis: " + String.valueOf(endResult));
+    }
+
+    // Summierung und Subtraktion
+    private int addAndSubstract(ArrayList<Integer> numbers)
+    {
+        int sum = 0;
+
+        // Subtrahiere die Zahl an der 3. Position 2 MAL - weil sie bei der Addition dazu gezählt wurde
+        sum = calculateSum(numbers) - 2 * numbers.get(2);
+
+        return sum;
+    }
+
+    // Füge die calculateSum Methode hier hinzu
+    private int calculateSum(ArrayList<Integer> numbers)
+    {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
 }
--- app/src/main/java/com/example/alzheimer_project/Village_Activity.java
+++ app/src/main/java/com/example/alzheimer_project/Village_Activity.java
@@ -2,41 +2,36 @@ package com.example.alzheimer_project;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class Village_Activity extends AppCompatActivity {
     private int counter = 0;
+    private ArrayList<Integer> randomNumbers = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_village);
 
-        // Initialisiere die TextView, um die zufällige Zahl anzuzeigen
         final TextView randomNumberTextView = findViewById(R.id.tv_random_number);
-
-        // Initialisiere die Buttons
         final Button btnChoice1 = findViewById(R.id.btn_choice1);
         final Button btnChoice2 = findViewById(R.id.btn_choice2);
 
-        // Setze die OnClickListener für die Buttons
         btnChoice1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Rufe generateRndNumTen() auf
-                int randomNumber = Village.generateRndNumTen();
-                // Zeige die zufällige Zahl in der TextView an
-                randomNumberTextView.setText(String.valueOf(randomNumber));
+                Village.setRandomNumberAndButtonText(btnChoice1, randomNumberTextView, randomNumbers);
 
-                // Überprüfe, ob fünf Wiederholungen erreicht wurden
                 if (++counter >= 5) {
-                    // Wechsle zur End_Activity
-                    startActivity(new Intent(Village_Activity.this, End_Activity.class));
-                    finish(); // Beende die aktuelle Activity
+                    Log.d("Village_Activity", "counter over 5 - choice 1");
+                    startEndActivity();
                 }
             }
         });
@@ -44,18 +39,20 @@ public class Village_Activity extends AppCompatActivity {
         btnChoice2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Rufe generateRndNumFive() auf
-                int randomNumber = Village.generateRndNumFive();
-                // Zeige die zufällige Zahl in der TextView an
-                randomNumberTextView.setText(String.valueOf(randomNumber));
+                Village.setRandomNumberAndButtonText(btnChoice2, randomNumberTextView, randomNumbers);
 
-                // Überprüfe, ob fünf Wiederholungen erreicht wurden
                 if (++counter >= 5) {
-                    // Wechsle zur End_Activity
-                    startActivity(new Intent(Village_Activity.this, End_Activity.class));
-                    finish(); // Beende die aktuelle Activity
+                    Log.d("Village_Activity", "counter over 5 - choice 2");
+                    startEndActivity();
                 }
             }
         });
+    }
+    private void startEndActivity() {
+        Log.d("Village_Activity", "startEndActivity started");
+        Intent intent = new Intent(Village_Activity.this, End_Activity.class);
+        intent.putIntegerArrayListExtra("randomNumbers", randomNumbers);
+        startActivity(intent);
+        finish();
     }
 }
