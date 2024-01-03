6073cba30cf22f9d5e3ea283c40e0f78ea52c9d2
greenfly-blub <63907390+greenfly-blub@users.noreply.github.com>
2024-01-03 20:23:48 +0100

    Result will be shown on a random button on end activity

--- app/src/main/java/com/example/alzheimer_project/End_Activity.java
+++ app/src/main/java/com/example/alzheimer_project/End_Activity.java
@@ -1,11 +1,13 @@
 package com.example.alzheimer_project;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class End_Activity extends AppCompatActivity {
     @Override
@@ -16,33 +18,44 @@ public class End_Activity extends AppCompatActivity {
         // Get the List with the numbers from the activity before
         ArrayList<Integer> numbers = getIntent().getIntegerArrayListExtra("randomNumbers");
 
-
-        // Call Add and Substract mehtod
-        int endResult = addAndSubstract(numbers);
+        // Call Add and Subtract method
+        int endResult = addAndSubtract(numbers);
 
         // Show End Result
         TextView specialSumTextView = findViewById(R.id.tv_math);
         specialSumTextView.setText("Ergebnis: " + String.valueOf(endResult));
+
+        // Get a random button index
+        int randomButtonIndex = getRandomButtonIndex();
+
+        // Set the endResult text on a random button
+        setEndResultButtonText(randomButtonIndex, endResult);
     }
 
-    // Summierung und Subtraktion
-    private int addAndSubstract(ArrayList<Integer> numbers)
-    {
-        int sum = 0;
-
+    // Summation and Subtraction
+    private int addAndSubtract(ArrayList<Integer> numbers) {
         // Subtrahiere die Zahl an der 3. Position 2 MAL - weil sie bei der Addition dazu gezählt wurde
-        sum = calculateSum(numbers) - 2 * numbers.get(2);
-
-        return sum;
+        return calculateSum(numbers) - 2 * numbers.get(2);
     }
 
     // Füge die calculateSum Methode hier hinzu
-    private int calculateSum(ArrayList<Integer> numbers)
-    {
+    private int calculateSum(ArrayList<Integer> numbers) {
         int sum = 0;
         for (int number : numbers) {
             sum += number;
         }
         return sum;
+    }
+
+    private int getRandomButtonIndex() {
+        Random random = new Random();
+        return random.nextInt(5) + 1;
+    }
+
+    // The right result will be shown as a button text on a random button
+    private void setEndResultButtonText(int buttonIndex, int endResult) {
+        int buttonId = getResources().getIdentifier("btn_option" + buttonIndex, "id", getPackageName());
+        Button selectedButton = findViewById(buttonId);
+        selectedButton.setText(String.valueOf(endResult));
     }
 }
